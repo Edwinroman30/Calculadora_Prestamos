3dd6376cfd8586cb9a874f3ead80b4db39377c7c
Edwin Roman <romanseberino@gmail.com>
2021-04-17 12:06:37 -0400

    En esta faceta del proyecto ya se programo la funcion que muesta la tabla de amortizacion

--- POO_PF.py
+++ POO_PF.py
@@ -1,5 +1,3 @@
-
-
 class Calculadora_Prestamos:
     
     def __init__(self):
@@ -27,18 +25,16 @@ class Calculadora_Prestamos:
 
 
     def General_tabla(self):
-        #'Pago: {0} | NULL{1} |Saldo Iniciar | CUOTA: {2} | Capital: {3} | Interes: {4} | Balance: {5}'
+        #'Pago: {0} | NULL{1} |Saldo Iniciar | CUOTA: {2} | Capital: {3} | Interes: {4} | Balance o Saldo Final: {5}'
         last_index = 0
         
         for x in range(1,(self.plazo+1),1):
-            
-            #Para resolver el error logico de indexacion   
-             
+                         
             if(x == 1):
-                saldo_inicial = self.monto 
-                intereses = (saldo_inicial*self.tasa_mensual)
-                capital =  (self.cuota - intereses) 
-                saldo_final = saldo_inicial - capital
+                saldo_inicial = round(self.monto,2) 
+                intereses = round((saldo_inicial*self.tasa_mensual),2) 
+                capital =  round((self.cuota - intereses) ,2) 
+                saldo_final = round(saldo_inicial - capital,2) 
                 
                 self.tabla.append([x,'NULL',saldo_inicial,self.cuota,capital,intereses,saldo_final])
                 
@@ -49,19 +45,19 @@ class Calculadora_Prestamos:
                 saldo_final = round(saldo_inicial - capital,2)
                 
                 self.tabla.append([x,'NULL',saldo_inicial,self.cuota,capital,intereses,saldo_final])         
-                
-#intereses = ()
-#capital =  (cuota - interes)             
-            
-        
+                           
     
-#    def Mostrar_tabla(self):
-#        for x in range(1,(n+1),1):
-#            print('Pago: {0} | {1} | CUOTA: {2} | Capital: {3} | Interes: {4} | Balance: {5}'.format(tabla[0],tabla[1],tabla[2],tabla[3],tabla[4],tabla[5]))   
+    def Mostrar_tabla(self):
+        
+        for x in range(0,(self.plazo),1):
+            print('Pago: {} | {} | CUOTA: {} | Capital: {} | Interes: {} | Balance: {}'
+                  .format(self.tabla[x][0], self.tabla[x][1] , self.tabla[x][3] , self.tabla[x][4] , self.tabla[x][5] , self.tabla[x][6]))   
 
 
 calculador = Calculadora_Prestamos()
 calculador.Entrada_data(287000,16.95,48)
 calculador.General_tabla()
-calculador.ver()
+#calculador.ver()
+calculador.Mostrar_tabla()
 
+#print(calculador.tabla[0])
