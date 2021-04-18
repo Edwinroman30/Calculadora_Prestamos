3e6a6cb3daf646a4e718dd0ea87ee595794cebc7
Edwin Roman <romanseberino@gmail.com>
2021-04-18 19:45:42 -0400

    En este commit se trabajo: En el algoritmo que permite mostrar las fechas, modificar el metodo [Entrada_data] para aceptar datos por teclado, se incluyo el metodo [Mostrar_tabla_amortizacion] usando el modulo tabulate y finalmente se elimino comentarios y variables redundantes.

--- POO_PF.py
+++ POO_PF.py
@@ -1,3 +1,6 @@
+from tabulate import tabulate
+from datetime import timedelta,datetime
+
 class Calculadora_Prestamos:
     
     def __init__(self):
@@ -6,58 +9,60 @@ class Calculadora_Prestamos:
         self.plazo = 0
         self.cuota = 0
         
-        self.tabla = []
+        self.tabla_amortizacion = []
         
             
-    def Entrada_data(self,p_monto,p_tasa,p_plazo):
+    def Entrada_data(self):
         #TOMAR ESTE PUNTO EN CUENTA PARA HACER LA ENTRADA POR TECLADO.
-        self.monto = p_monto
-        self.tasa_mensual = ((p_tasa/12)/100)
-        self.plazo = p_plazo
+        self.monto = float(input('Monto del prestamo en RD$: '))
+        #converTasa = ((float(input('Monto del prestamo en RD$: '))/12)/100)
+        self.tasa_mensual = ((float(input('Tasa de Porcentaje Anual: '))/12)/100)
+        self.plazo = int(input('Plazo en meses: '))
         
         self.cuota = round( ((self.monto * self.tasa_mensual) / (1 - (1+self.tasa_mensual)**-self.plazo)) ,2)
    
-    #!Codigo TEMPORAL
-    def ver(self):
-        print('CUota de ',self.cuota)
-        print(self.tasa_mensual)
-        print(self.tabla)
+    
+    #Muestra la tabla_amortizacion
+    def Mostrar_tabla_amortizacion(self):
+        print(tabulate(self.tabla_amortizacion, floatfmt='.2f',headers=["Pago","Fecha Pago","Saldo Iniciar","Cuota","Capital","Interes","Balance"], tablefmt='fancy_grid'))
 
-
-    def General_tabla(self):
-        #'Pago: {0} | NULL{1} |Saldo Iniciar | CUOTA: {2} | Capital: {3} | Interes: {4} | Balance o Saldo Final: {5}'
-        last_index = 0
+    
+    #Metodo nucleo que me permite general los calculos.
+    def General_tabla_amortizacion(self):
         
+        #Para poder trabajar con la fecha.
+        formato_fecha = "%d-%b-%Y"
+        fecha_prestamo = datetime.now()
+
+
         for x in range(1,(self.plazo+1),1):
-                         
+            
+            #Por cada ciclo sumo un mes a la fecha actuar.
+            fecha_prestamo = fecha_prestamo + timedelta(days=+30)
+                          
             if(x == 1):
                 saldo_inicial = round(self.monto,2) 
                 intereses = round((saldo_inicial*self.tasa_mensual),2) 
                 capital =  round((self.cuota - intereses) ,2) 
                 saldo_final = round(saldo_inicial - capital,2) 
                 
-                self.tabla.append([x,'NULL',saldo_inicial,self.cuota,capital,intereses,saldo_final])
+                self.tabla_amortizacion.append([x,fecha_prestamo.strftime(formato_fecha),saldo_inicial,self.cuota,capital,intereses,saldo_final])
                 
             else:
-                saldo_inicial = round(self.tabla[-1][6],2) 
+                saldo_inicial = round(self.tabla_amortizacion[-1][6],2) 
                 intereses = round((saldo_inicial*self.tasa_mensual),2) 
                 capital =  round((self.cuota - intereses),2)  
                 saldo_final = round(saldo_inicial - capital,2)
                 
-                self.tabla.append([x,'NULL',saldo_inicial,self.cuota,capital,intereses,saldo_final])         
-                           
+                self.tabla_amortizacion.append([x, fecha_prestamo.strftime(formato_fecha) ,saldo_inicial,self.cuota,capital,intereses,saldo_final])         
+                #FORMATO DE INSERCION EN LA TABLA => 'Pago: {0} | Fecha |Saldo Iniciar | CUOTA: {2} | Capital: {3} | Interes: {4} | Balance o Saldo Final: {5}'
+                   
     
-    def Mostrar_tabla(self):
-        
-        for x in range(0,(self.plazo),1):
-            print('Pago: {} | {} | CUOTA: {} | Capital: {} | Interes: {} | Balance: {}'
-                  .format(self.tabla[x][0], self.tabla[x][1] , self.tabla[x][3] , self.tabla[x][4] , self.tabla[x][5] , self.tabla[x][6]))   
+  
 
 
+#INSTANCIANDO LA CLASE
 calculador = Calculadora_Prestamos()
-calculador.Entrada_data(287000,16.95,48)
-calculador.General_tabla()
-#calculador.ver()
-calculador.Mostrar_tabla()
-
-#print(calculador.tabla[0])
+calculador.Entrada_data()
+calculador.General_tabla_amortizacion()
+calculador.Mostrar_tabla_amortizacion()
